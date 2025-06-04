f66c73ff1ee07aa5e5fe3fde47faa7839232cccf
Salvatore Allegra <salallegra@gmail.com>
2025-06-04 02:55:52 -0700

    add AppDbContext, and some annotations to coupon model

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; } = null!;
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon
+                {
+                    CouponId = 1,
+                    CouponCode = "10OFF",
+                    DiscountAmount = 10,
+                    MinAmount = 100
+                },
+                new Coupon
+                {
+                    CouponId = 2,
+                    CouponCode = "20OFF",
+                    DiscountAmount = 20,
+                    MinAmount = 200
+                }
+            );
+       */ }
+    }
+}
